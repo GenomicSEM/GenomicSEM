3cb02874d1df0be851aacfc4e38c1807813c3c6f
MichelNivard <m.g.nivard@gmail.com>
2018-04-21 17:07:28 +0200

    Update munge.R

--- R/munge.R
+++ R/munge.R
@@ -35,22 +35,22 @@ munge <- function(files,hm3,trait.names=NULL,N,info.filter = .9,maf.filter=0.01)
     
      
     
-    if(sum(hold_names %in% "P") == 0) print(paste0('Cannot find P-value column, try renaming it "P" in the summary statistics file for:',filenames[i]))
-    if(sum(hold_names %in% "A1") == 0) print(paste0('Cannot find effect allele column, try renaming it "A1" in the summary statistics file for:',filenames[i]))
-    if(sum(hold_names %in% "A2") == 0) print(paste0('Cannot find other allele column, try renaming it "A2" in the summary statistics file for:',filenames[i]))
-    if(sum(hold_names %in% "effect") == 0) print(paste0('Cannot find beta or effect column, try renaming it "effect" in the summary statistics file for:',filenames[i]))
-    if(sum(hold_names %in% "effect") == 0) print(paste0('Cannot rs-id column, try renaming it "SNP" in the summary statistics file for:',filenames[i]))
+    if(sum(hold_names %in% "P") == 0) print(paste0('Cannot find P-value column, try renaming it P in the summary statistics file for:',filenames[i]))
+    if(sum(hold_names %in% "A1") == 0) print(paste0('Cannot find effect allele column, try renaming it A1 in the summary statistics file for:',filenames[i]))
+    if(sum(hold_names %in% "A2") == 0) print(paste0('Cannot find other allele column, try renaming it A2 in the summary statistics file for:',filenames[i]))
+    if(sum(hold_names %in% "effect") == 0) print(paste0('Cannot find beta or effect column, try renaming it effect in the summary statistics file for:',filenames[i]))
+    if(sum(hold_names %in% "effect") == 0) print(paste0('Cannot rs-id column, try renaming it"SNP in the summary statistics file for:',filenames[i]))
                                                            
                                                
     
     # Throw warnings for misisng P valuue, rs, effect or allele columns
   
     
-    if(sum(hold_names %in% "P") == 0) warning(paste0('Cannot find P-value column, try renaming it "P" in the summary statistics file for:',filenames[i]))
-    if(sum(hold_names %in% "A1") == 0) warning(paste0('Cannot find effect allele column, try renaming it "A1" in the summary statistics file for:',filenames[i]))
-    if(sum(hold_names %in% "A2") == 0) warning(paste0('Cannot find other allele column, try renaming it "A2" in the summary statistics file for:',filenames[i]))
-    if(sum(hold_names %in% "effect") == 0) warning(paste0('Cannot find beta or effect column, try renaming it "effect" in the summary statistics file for:',filenames[i]))
-    if(sum(hold_names %in% "effect") == 0) warning(paste0('Cannot rs-id column, try renaming it "SNP" in the summary statistics file for:',filenames[i]))
+    if(sum(hold_names %in% "P") == 0) warning(paste0('Cannot find P-value column, try renaming it P in the summary statistics file for:',filenames[i]))
+    if(sum(hold_names %in% "A1") == 0) warning(paste0('Cannot find effect allele column, try renaming it A1 in the summary statistics file for:',filenames[i]))
+    if(sum(hold_names %in% "A2") == 0) warning(paste0('Cannot find other allele column, try renaming it A2 in the summary statistics file for:',filenames[i]))
+    if(sum(hold_names %in% "effect") == 0) warning(paste0('Cannot find beta or effect column, try renaming it effect in the summary statistics file for:',filenames[i]))
+    if(sum(hold_names %in% "effect") == 0) warning(paste0('Cannot rs-id column, try renaming it SNP in the summary statistics file for:',filenames[i]))
                                                            
                                                
     
