16650e4c03bc757ce0d0d467c6c2de21923c20d9
agrotzin <33735206+agrotzin@users.noreply.github.com>
2018-05-18 18:45:39 -0500

    Update sumstats.R

--- R/sumstats.R
+++ R/sumstats.R
@@ -58,7 +58,11 @@ sumstats <- function(files,ref,trait.names=NULL,se.logit,OLS=FALSE,linprob=FALSE
     ##determine whether it is OR or logistic/continuous effect based on median effect size 
     files[[i]]$effect<-ifelse(rep(round(median(files[[i]]$effect)) == 1,nrow(files[[i]])), log(files[[i]]$effect),files[[i]]$effect)
     
-    
+    ##remove any rows with missing p-values
+      if("P" %in% colnames(files[[i]])) {
+   files[[i]]<-subset(files[[i]], !(is.na(files[[i]]$P)))
+    }
+   
       if(OLS[i] == T){
       
       files[[i]]$Z <- sign(files[[i]]$effect) * sqrt(qchisq(files[[i]]$P,1,lower=F))
