9e3897fce4550f8a354ae837537a1301a1009d40
agrotzin <33735206+agrotzin@users.noreply.github.com>
2018-07-15 13:14:31 +0200

    Update sumstats.R

--- R/sumstats.R
+++ R/sumstats.R
@@ -38,17 +38,14 @@ sumstats <- function(files,ref,trait.names=NULL,se.logit,OLS=NULL,linprob=NULL,p
     hold_names <- names(files[[i]])
     
     hold_names[hold_names %in%c("snp","SNP","snpid","SNPID","rsid","RSID","RS_NUMBER","rs_number","RS_NUMBERS","rs_numbers","MarkerName","Markername", "markername", "MARKERNAME")] <- "SNP"
-    hold_names[hold_names %in%c("a1","A1","allele1","Allele1", "ALLELE1","Tested_Allele","EFFECT_ALLELE","INC_ALLELE","REFERENCE_ALLELE","EA","Effect_allele")] <- "A1"
-    hold_names[hold_names %in%c("a2","A2","allele2","Allele2","ALLELE2","OTHER_ALLELE","NON_EFFECT_ALLELE","DEC_ALLELE","NEA","Other_allele","Other_Allele")]  <- "A2"
-    hold_names[hold_names %in%c("OR","or","B","beta","BETA","Beta", "LOG_ODDS","EFFECTS","EFFECT","SIGNED_SUMSTAT", "Effect")] <- "effect"
-    hold_names[hold_names %in%c("se","StdErr","SE")] <- "SE"
+    hold_names[hold_names %in%c("a1","A1","allele1","Allele1", "ALLELE1","EFFECT_ALLELE","INC_ALLELE","REFERENCE_ALLELE","EA","Effect_allele", "Effect_Allele")] <- "A1"
+    hold_names[hold_names %in%c("a2","A2","allele2","Allele2","ALLELE2","OTHER_ALLELE","NON_EFFECT_ALLELE","DEC_ALLELE","NEA","Other_allele", "Other_Allele")]  <- "A2"
+    hold_names[hold_names %in%c("OR","or","B","Beta","beta","BETA","LOG_ODDS","EFFECTS","EFFECT","SIGNED_SUMSTAT", "Effect","Z","Zscore","b")] <- "effect"
     hold_names[hold_names %in%c("INFO","info")] <- "INFO"
-    hold_names[hold_names %in%c("P","p","PVALUE","Pval","pvalue","P_VALUE","p_value","PVAL","pval","P_VAL","p_val","GC_PVALUE","gc_pvalue" )] <- "P"
-    hold_names[hold_names %in%c("N","WEIGHT","nCompleteSamples")] <- "N"
+    hold_names[hold_names %in%c("P","p","PVALUE","Pval","pvalue","P_VALUE","P_value","P-value","p-value","P-value","p_value","PVAL","pval","P_VAL","p_val","GC_PVALUE","gc_pvalue", "P_Value")] <- "P"
+    hold_names[hold_names %in%c("N","WEIGHT","nCompleteSamples", "TotalSampleSize")] <- "N"
     hold_names[hold_names %in%c("NCASE","N_CASE","N_CASES","N_CAS")] <- "N_CAS"
     hold_names[hold_names %in%c("NCONTROL","N_CONTROL","N_CONTROLS","N_CON","CONTROLS_N")] <- "N_CON"
-    
-   
     
     
     # Print a message for misisng P value, rs, effect or allele columns
