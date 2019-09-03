22e2ab441d5cb6475bbc9571bb7effce05dbbe8a
agrotzin <33735206+agrotzin@users.noreply.github.com>
2019-09-03 18:20:54 -0500

    Update userGWASpar.R

--- R/userGWASpar.R
+++ R/userGWASpar.R
@@ -336,7 +336,7 @@ userGWASpar<-function(Output,estimation="DWLS",model="",modelchi=FALSE,printwarn
         ##run the model. save failed runs and run model. warning and error functions prevent loop from breaking if there is an error. 
         test<-tryCatch.W.E(Model1_Results <- sem(Model1, sample.cov = S_Fullrun, estimator = "DWLS", WLS.V = W, sample.nobs = 2, optim.dx.tol = +Inf))
         
-        if(class(test$value)[1] == "lavaan"){
+        if(class(test$value)[1] == "lavaan" & grepl("solution has NOT",  as.character(test$warning)) != TRUE){
         Model_WLS <- parTable(Model1_Results)
         
         if(NA %in% Model_WLS$se){
