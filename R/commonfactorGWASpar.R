ff66aefe9b4024abf9b60d14a4f3c45165dd3304
MichelNivard <m.g.nivard@gmail.com>
2019-11-27 08:15:08 +0100

    Update commonfactorGWASpar.R

--- R/commonfactorGWASpar.R
+++ R/commonfactorGWASpar.R
@@ -5,13 +5,12 @@ commonfactorGWASpar <-function(covstruc=NULL,SNPs=NULL,estimation="DWLS",cores=N
   print("Please note that an update was made to commonfactorGWASpar on 11/21/19 so that it combines addSNPs and commonfactorGWASpar.")
   
   
-  if(!is.null(covstruc)){
-  if(class(covstruc[[1]]) != "matrix"){
-    print("You are likely listing arguments in the order of a previous version of commonfactorGWAS. The current version of the function expects the 
-          output from ldsc followed by the output from sumstats as the first two arguments.")    
-    warning("You are likely listing arguments in the order of a previous version of commonfactorGWAS. The current version of the function expects the 
-            output from ldsc followed by the output from sumstats as the first two arguments.")
-  }}
+ if(class(SNPs) == "character" | is.null(SNPs) | is.null(covstruct)){
+    print("You are likely listing arguments in the order of a previous version of commonfactorGWASpar, if you have yur results stored after running addSNPs you can still explicitly call Output = ... to proviide them to commonfactorGWASpar. The current version of the function is way faster and expects the 
+          output from ldsc followed by the output from sumstats (using SNPs = ... ) as the first two arguments. See ?commonfactorGWASpar for help on propper usage")    
+    stop("You are likely listing arguments (e.g. Output = ...) in the order of a previous version of commonfactorGWASpar, if you have yur results stored after running addSNPs you can still explicitly call Output = ... to proviide them to commonfactorGWASpar. The current version of the function is way faster and expects the 
+            output from ldsc (using covstruct = ...)  followed by the output from sumstats (using SNPs = ... ) as the first two arguments. See ?commonfactorGWASpar for help on propper usage")
+  }
   
   
   if(is.null(cores)){
