c7cce8e713526a368e16dda74a9dbe6262bdf6ba
Matthijs <m.d.vander.zee@vu.nl>
2022-02-04 09:33:19 +0100

    Code update 04.02.2022

--- R/sumstats_main.R
+++ R/sumstats_main.R
@@ -20,11 +20,11 @@
 
   .LOG("Preparing summary statistics for file: ", filename,file=log.file)
   N_provided <- (!is.na(N))
-  if ((se.logit) | (all(!(c(linprob, OLS, se.logit))))) {
+  if (linprob){
+    stop_on_missing <- c("effect", "SNP")
+  }  else if ((se.logit) | (all(!(c(linprob, OLS, se.logit))))) {
     # if se.logit == T, or linprob,OLS,se.logit are all FALSE, SE is required.
     stop_on_missing <- c("effect", "SNP", "SE")
-  } else if (linprob){
-    stop_on_missing <- c("effect", "SNP")
   } else {
     stop_on_missing <- c("effect", "SNP")
   }
