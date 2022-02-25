367d512f3940f575716216067aa4378ffc92330c
agrotzin <33735206+agrotzin@users.noreply.github.com>
2022-02-25 09:04:07 -0700

    Update sumstats_main.R

--- R/sumstats_main.R
+++ R/sumstats_main.R
@@ -62,7 +62,7 @@
     if("DIRECTION" %in% colnames(file)) {
       b<-nrow(file)
       file<-subset(file, (str_count(file$DIRECTION, "\\?")/nchar(file$DIRECTION)) < .5)
-      .LOG((b-nrow(file)), "rows were removed from the", filename, "summary statistics file due to missingness across > 50% of contributing cohorts as as determined by missing info in the direction column.",file=log.file)
+      .LOG((b-nrow(file)), "rows were removed from the", filename, "summary statistics file due to missingness across > 50% of contributing cohorts as determined by missing info in the direction column.",file=log.file)
     }else{.LOG("No DIRECTION column, cannot filter on missingness by conributing cohorts, which may influence results",file=log.file)}
   }
   
