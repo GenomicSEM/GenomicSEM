dbe713c28c63d38739852a9aadb783dae71f2c41
agrotzin <33735206+agrotzin@users.noreply.github.com>
2023-01-21 17:51:44 -0700

    fix typo in warning messages

--- R/sumstats_main.R
+++ R/sumstats_main.R
@@ -63,7 +63,7 @@
       b<-nrow(file)
       file$DIRECTION<-as.character(file$DIRECTION)
       file<-subset(file, (str_count(file$DIRECTION, "\\?")/nchar(file$DIRECTION)) < .5)
-      .LOG((b-nrow(file)), "rows were removed from the", filename, "summary statistics file due to missingness across > 50% of contributing cohorts as determined by missing info in the direction column.",file=log.file)
+      .LOG((b-nrow(file)), "rows were removed from the", filename, "summary statistics file due to missingness across 50% or more of contributing cohorts as determined by missing info in the direction column.",file=log.file)
     }else{.LOG("No DIRECTION column, cannot filter on missingness by conributing cohorts, which may influence results",file=log.file)}
   }
   
