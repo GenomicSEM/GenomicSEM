ad8ded8b33f3a20b1158d2c1a1e2ed410029698b
agrotzin <33735206+agrotzin@users.noreply.github.com>
2023-03-24 08:25:31 -0600

    bug fix causing commonfactorGWAS to error out
    Recent update to lavaan was adding additional rows to the internal representation of the model that was causing the follow-up independent pathways model used for Q_SNP estimation to fail out. Code update reflects fix to exclude those additional rows.

--- R/commonfactorGWAS_main.R
+++ R/commonfactorGWAS_main.R
@@ -133,6 +133,9 @@
     ##code to estimate Q_SNP##
     #First pull the estimates from Step 1
     ModelQ <- parTable(Model1_Results)
+    
+    #2023 add: remove additional rows for internal representation of model by lavaan
+    ModelQ<-ModelQ[1:((k+1)*(k+2))/2,]
 
     #fix the indicator loadings from Step 1, free the direct effects of the SNP on the indicators, and fix the factor residual variance
     ModelQ$free <- c(rep(0, k+1), 1:(k*2), 0, 0)
