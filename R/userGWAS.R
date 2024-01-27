8142679454b167539893560d4d4b97fc20b4d885
agrotzin <33735206+agrotzin@users.noreply.github.com>
2024-01-27 06:28:11 -0700

    Update userGWAS.R

--- R/userGWAS.R
+++ R/userGWAS.R
@@ -1,17 +1,13 @@
 userGWAS <- function(covstruc=NULL, SNPs=NULL, estimation="DWLS", model="", printwarn=TRUE,
                      sub=FALSE,cores=NULL, toler=FALSE, SNPSE=FALSE, parallel=TRUE, GC="standard", MPI=FALSE,
-                     smooth_check=FALSE, TWAS=FALSE, std.lv=FALSE,fix_measurement=TRUE){
+                     smooth_check=FALSE, TWAS=FALSE, std.lv=FALSE,fix_measurement=TRUE,Q_SNP=FALSE){
+  
   # Set toler to machine precision to enable passing this to solve() directly
   if (!toler) toler <- .Machine$double.eps
-  # Sanity checks
-  #TODO: add check for covstruc
-  #TODO: add check for SNPs
-  #TODO: add check for sub
   .check_one_of(estimation, c("DWLS", "ML"))
   .check_boolean(printwarn)
   if (!is.null(cores)) .check_range(cores, min=0, max=Inf)
   .check_range(toler, min=0, max=Inf)
-  # .check_boolean(SNPSE)
   .check_boolean(parallel)
   .check_one_of(GC, c("standard", "conserv", "none"))
   .check_boolean(MPI)
@@ -77,19 +73,14 @@ userGWAS <- function(covstruc=NULL, SNPs=NULL, estimation="DWLS", model="", prin
   #small number because treating MAF as fixed
   if(SNPSE == FALSE){
     varSNPSE2 <- (.0005)^2
-  }
-  
-  if(SNPSE != FALSE){
-    varSNPSE2 <- SNPSE^2
-  }
-  
+  }else{varSNPSE2 <- SNPSE^2}
+
   V_LD <- as.matrix(covstruc[[1]])
   S_LD <- as.matrix(covstruc[[2]])
   I_LD <- as.matrix(covstruc[[3]])
   Model1 <- model
   
-  for(i in 1){
-    if(fix_measurement){
+  if(fix_measurement){
       
       #name rownames as column names
       rownames(S_LD)<-colnames(S_LD)
@@ -99,15 +90,15 @@ userGWAS <- function(covstruc=NULL, SNPs=NULL, estimation="DWLS", model="", prin
       
       # Use grep to find lines containing "SNP" or "Gene" and exclude them
       if(TWAS){
-      filtered_lines <- lines[!grepl("Gene", lines)]
+        filtered_lines <- lines[!grepl(c("Gene"), lines)]
       }else{filtered_lines <- lines[!grepl("SNP", lines)]}
-
+      
       #remove ghost parameters as this could include SNP/Gene effects 
       filtered_lines<-filtered_lines[!grepl(c(":="), filtered_lines)]
       
       # Join the filtered lines back into a single text string
       noSNPmodel <- paste(filtered_lines, collapse = "\n")
-
+      
       #smooth S and V matrices if necessary
       smoothS<-ifelse(eigen(S_LD)$values[nrow(S_LD)] <= 0, S_LD<-as.matrix((nearPD(S_LD, corr = FALSE))$mat), S_LD<-S_LD)
       smoothV<-ifelse(eigen(V_LD)$values[nrow(V_LD)] <= 0, V_LD<-as.matrix((nearPD(V_LD, corr = FALSE))$mat), V_LD<-V_LD)
@@ -135,23 +126,11 @@ userGWAS <- function(covstruc=NULL, SNPs=NULL, estimation="DWLS", model="", prin
       #estimate the measurement model
       if(estimation == "DWLS"){
         ##run the model. save failed runs and run model. warning and error functions prevent loop from breaking if there is an error. 
-        if(std.lv == FALSE){
-          emptynoSNP<-.tryCatch.W.E(Model1_Results <- sem(noSNPmodel, sample.cov = S_LD, estimator = "DWLS", WLS.V = W_Reorder, sample.nobs = 2,optim.dx.tol = +Inf))
-        }
-        
-        if(std.lv == TRUE){
-          emptynoSNP<-.tryCatch.W.E(Model1_Results <- sem(noSNPmodel, sample.cov = S_LD, estimator = "DWLS", WLS.V = W_Reorder, sample.nobs = 2,std.lv=TRUE, optim.dx.tol = +Inf))
-        }
+        emptynoSNP<-.tryCatch.W.E(Model1_Results <- sem(noSNPmodel, sample.cov = S_LD, estimator = "DWLS", WLS.V = W_Reorder, sample.nobs = 2,optim.dx.tol = +Inf,std.lv=std.lv))
       }
       
       if(estimation == "ML"){
-        if(std.lv == FALSE){
-          emptynoSNP<-.tryCatch.W.E(Model1_Results <- sem(noSNPmodel, sample.cov = S_LD, estimator = "ML",  sample.nobs = 200,optim.dx.tol = +Inf,sample.cov.rescale=FALSE))
-        }
-        
-        if(std.lv == TRUE){
-          emptynoSNP<-.tryCatch.W.E(Model1_Results <- sem(noSNPmodel, sample.cov = S_LD, estimator = "ML", sample.nobs = 200,std.lv=TRUE, optim.dx.tol = +Inf,sample.cov.rescale=FALSE))
-        }
+          emptynoSNP<-.tryCatch.W.E(Model1_Results <- sem(noSNPmodel, sample.cov = S_LD, estimator = "ML",  sample.nobs = 200,optim.dx.tol = +Inf,sample.cov.rescale=FALSE,std.lv=std.lv))
       }
       
       #pull the model results
@@ -163,7 +142,6 @@ userGWAS <- function(covstruc=NULL, SNPs=NULL, estimation="DWLS", model="", prin
       }
       
     }
-  }
   
   beta_SNP <- SNPs[,grep("beta.",fixed=TRUE,colnames(SNPs))]
   SE_SNP <- SNPs[,grep("se.",fixed=TRUE,colnames(SNPs))]
@@ -175,56 +153,15 @@ userGWAS <- function(covstruc=NULL, SNPs=NULL, estimation="DWLS", model="", prin
   diag(I_LD) <- ifelse(diag(I_LD)<= 1, 1, diag(I_LD))
   
   coords <- which(I_LD != 'NA', arr.ind= T)
-  i <- 1  
-  #create empty shell of V_SNP matrix
-  V_SNP <- diag(n_phenotypes)
   
-  #loop to add in the GWAS SEs, correct them for univariate and bivariate intercepts, and multiply by SNP variance from reference panel
-  for (p in 1:nrow(coords)) {
-    x <- coords[p,1]
-    y <- coords[p,2]
-    if (x != y) {
-      V_SNP[x,y] <- (SE_SNP[i,y]*SE_SNP[i,x]*I_LD[x,y]*I_LD[x,x]*I_LD[y,y]*varSNP[i]^2)}
-    if (x == y) {
-      V_SNP[x,x] <- (SE_SNP[i,x]*I_LD[x,x]*varSNP[i])^2
-    }
-  }
+  V_SNP <- .get_V_SNP(SE_SNP, I_LD, varSNP, GC, coords, n_phenotypes, 1)
   
   V_full <- .get_V_full(n_phenotypes, V_LD, varSNPSE2, V_SNP)
   
   kv <- nrow(V_full)
   smooth2 <- ifelse(eigen(V_full)$values[kv] <= 0, V_full <- as.matrix((nearPD(V_full, corr = FALSE))$mat), V_full <- V_full)
   
-  #create empty vector for S_SNP
-  S_SNP <- vector(mode="numeric",length=n_phenotypes+1)
-  
-  #enter SNP variance from reference panel as first observation
-  S_SNP[1] <- varSNP[i]
-  
-  #enter SNP covariances (standardized beta * SNP variance from refference panel)
-  for (p in 1:n_phenotypes) {
-    S_SNP[p+1] <- varSNP[i]*beta_SNP[i,p]
-  }
-  
-  #create shell of the full S (observed covariance) matrix
-  S_Full <- diag(n_phenotypes+1)
-  
-  ##add the LD portion of the S matrix
-  S_Full[(2:(n_phenotypes+1)),(2:(n_phenotypes+1))] <- S_LD
-  
-  ##add in observed SNP variances as first row/column
-  S_Full[1:(n_phenotypes+1),1] <- S_SNP
-  S_Full[1,1:(n_phenotypes+1)] <- t(S_SNP)
-  
-  ##pull in variables names specified in LDSC function and name first column as SNP
-  if(TWAS){
-    colnames(S_Full) <- c("Gene", colnames(S_LD))
-  } else {
-    colnames(S_Full) <- c("SNP", colnames(S_LD))
-  }
-  
-  ##name rows like columns
-  rownames(S_Full) <- colnames(S_Full)
+  S_Full<-.get_S_Full(n_phenotypes,S_LD,varSNP,beta_SNP,TWAS,1)
   
   ##smooth to near positive definite if either V or S are non-positive definite
   ks <- nrow(S_Full)
@@ -244,23 +181,23 @@ userGWAS <- function(covstruc=NULL, SNPs=NULL, estimation="DWLS", model="", prin
     if(fix_measurement){
       #pull the model with SNP effects
       withSNP<-parTable(ReorderModel)
-
+      
       #rbind back in Gene/SNP effects
       if(TWAS){
-          for(p in 1:nrow(withSNP)){
-        if(withSNP$rhs[p] == "Gene" | withSNP$lhs[p] == "Gene"){
-          Model1<-rbind(Model1,withSNP[p,])
+        for(p in 1:nrow(withSNP)){
+          if(withSNP$rhs[p] == "Gene" | withSNP$lhs[p] == "Gene"){
+            Model1<-rbind(Model1,withSNP[p,])
+          }
+        }
+      }else{ 
+        for(p in 1:nrow(withSNP)){
+          if(withSNP$rhs[p] == "SNP" | withSNP$lhs[p] == "SNP"){
+            Model1<-rbind(Model1,withSNP[p,])
+          }
         }
       }
-        }else{ 
-      for(p in 1:nrow(withSNP)){
-        if(withSNP$rhs[p] == "SNP" | withSNP$lhs[p] == "SNP"){
-          Model1<-rbind(Model1,withSNP[p,])
-        }
-      }
-        }
-
-        #rbind back in ghost parameters if relevant
+      
+      #rbind back in ghost parameters if relevant
       for(p in 1:nrow(withSNP)){
         if(withSNP$op[p] == ":="){
           Model1<-rbind(Model1,withSNP[p,])
@@ -282,16 +219,15 @@ userGWAS <- function(covstruc=NULL, SNPs=NULL, estimation="DWLS", model="", prin
   }
   
   if(TWAS){
-    SNPs2 <- SNPs[,1:3]
+    SNPs <- SNPs[,1:3]
   } else {
-    SNPs2 <- SNPs[,1:6]
+    SNPs <- SNPs[,1:6]
   }
   
-  rm(SNPs)
   f <- nrow(beta_SNP)
   # Run a single SNP to obtain base Lavaan model object
-  LavModel1 <- .userGWAS_main(i=1, cores=1, n_phenotypes, n=1, I_LD, V_LD, S_LD, std.lv, varSNPSE2, order, SNPs2, beta_SNP, SE_SNP, varSNP, GC,
-                              coords, smooth_check, TWAS, printwarn, toler, estimation, sub, Model1, df, npar, returnlavmodel=TRUE)
+  LavModel1 <- .userGWAS_main(i=1, cores=1, n_phenotypes, n=1, I_LD, V_LD, S_LD, std.lv, varSNPSE2, order, SNPs, beta_SNP, SE_SNP, varSNP, GC,
+                              coords, smooth_check, TWAS, printwarn, toler, estimation, sub, Model1, df, npar, returnlavmodel=TRUE,Q_SNP=Q_SNP,model=model)
   if(!parallel){
     #make empty list object for model results if not saving specific model parameter
     if(sub[[1]]==FALSE){
@@ -312,8 +248,8 @@ userGWAS <- function(covstruc=NULL, SNPs=NULL, estimation="DWLS", model="", prin
           cat(paste0("Running Model: ", i, "\n"))
         }
       }
-      final2 <- .userGWAS_main(i, cores=1, n_phenotypes, 1, I_LD, V_LD, S_LD, std.lv, varSNPSE2, order, SNPs2, beta_SNP, SE_SNP, varSNP, GC,
-                               coords, smooth_check, TWAS, printwarn, toler, estimation, sub, Model1, df, npar, basemodel=LavModel1)
+      final2 <- .userGWAS_main(i, cores=1, n_phenotypes, 1, I_LD, V_LD, S_LD, std.lv, varSNPSE2, order, SNPs, beta_SNP, SE_SNP, varSNP, GC,
+                               coords, smooth_check, TWAS, printwarn, toler, estimation, sub, Model1, df, npar, basemodel=LavModel1,Q_SNP=Q_SNP,model=model)
       final2$i <- NULL
       if(sub[[1]] != FALSE){
         final3 <- as.data.frame(matrix(NA,ncol=ncol(final2),nrow=length(sub)))
@@ -365,7 +301,7 @@ userGWAS <- function(covstruc=NULL, SNPs=NULL, estimation="DWLS", model="", prin
     }
     
     #split the V_SNP and S_SNP matrices into as many (cores - 1) as are aviailable on the local computer
-    SNPs2 <- suppressWarnings(split(SNPs2,1:int))
+    SNPs <- suppressWarnings(split(SNPs,1:int))
     beta_SNP <- suppressWarnings(split(beta_SNP,1:int))
     SE_SNP <- suppressWarnings(split(SE_SNP,1:int))
     varSNP <- suppressWarnings(split(varSNP,1:int))
@@ -379,8 +315,8 @@ userGWAS <- function(covstruc=NULL, SNPs=NULL, estimation="DWLS", model="", prin
       results <- foreach(n = icount(int), .combine = 'rbind') %:%
         foreach (i=1:nrow(beta_SNP[[n]]), .combine='rbind', .packages = "lavaan") %dopar% 
         .userGWAS_main(i, int, n_phenotypes, n, I_LD, V_LD, S_LD,
-                       std.lv, varSNPSE2, order, SNPs2[[n]], beta_SNP[[n]], SE_SNP[[n]], varSNP[[n]], GC,
-                       coords, smooth_check, TWAS, printwarn, toler, estimation, sub, Model1, df, npar, basemodel=LavModel1)
+                       std.lv, varSNPSE2, order, SNPs[[n]], beta_SNP[[n]], SE_SNP[[n]], varSNP[[n]], GC,
+                       coords, smooth_check, TWAS, printwarn, toler, estimation, sub, Model1, df, npar, basemodel=LavModel1,Q_SNP=Q_SNP,model=model)
     } else {
       #Util-functions have to be explicitly passed to the analysis function in PSOCK cluster
       utilfuncs <- list()
@@ -392,9 +328,9 @@ userGWAS <- function(covstruc=NULL, SNPs=NULL, estimation="DWLS", model="", prin
         foreach (i=1:nrow(beta_SNP[[n]]), .combine='rbind', .packages = c("lavaan", "gdata"),
                  .export=c(".userGWAS_main")) %dopar% {
                    .userGWAS_main(i, int, n_phenotypes, n, I_LD, V_LD, S_LD, std.lv, varSNPSE2, order,
-                                  SNPs2[[n]], beta_SNP[[n]], SE_SNP[[n]], varSNP[[n]], GC, coords,
+                                  SNPs[[n]], beta_SNP[[n]], SE_SNP[[n]], varSNP[[n]], GC, coords,
                                   smooth_check, TWAS, printwarn, toler, estimation, sub, Model1,
-                                  df, npar, utilfuncs, basemodel=LavModel1)
+                                  df, npar, utilfuncs, basemodel=LavModel1,Q_SNP=Q_SNP,model=model)
                  }
     }
     
